a384895ec63e1b1eaa4502bd38d0c046e4f67154
YoungJae Kim <yjkim1803@gmail.com>
2024-12-12 00:27:46 +0900

    fix: prevent scrolling when modal is open

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,6 +1,14 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 import { createPortal } from "react-dom";
 
 export default function Modal({ children }: PropsWithChildren) {
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    }
+  }, [])
+
   return createPortal(<div className="fixed flex justify-center items-center left-0 top-0 w-full h-full bg-white z-30">{children}</div>, document.body)
 }
