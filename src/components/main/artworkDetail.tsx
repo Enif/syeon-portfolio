380a9371a44724cb38b832ab302876b5f6f01e62
YoungJae Kim <yjkim1803@gmail.com>
2024-12-12 12:30:05 +0900

    feat: add placholder blur on artwork detail

--- src/components/main/artworkDetail.tsx
+++ src/components/main/artworkDetail.tsx
@@ -23,7 +23,7 @@ export default function ArtworkDetail({ artworkDetails }: Props) {
       <button className="border-2 border-main-gray text-main-gray disabled:border-main-gray100 disabled:text-main-gray100 w-[62px] h-[62px] rounded-full flex justify-center items-center" onClick={() => setCurrentDetailIndex(currentDetailIndex - 1)} disabled={!canMovePrev}>
         <ChevronLeft />
       </button>
-      {currentDetail.type === 'image' && <Image src={currentDetail.src} alt={currentDetail.alt} width={1320} height={915} />}
+      {currentDetail.type === 'image' && <Image src={currentDetail.src} alt={currentDetail.alt} width={1320} height={915} placeholder="blur" />}
       {currentDetail.type === 'video' && <iframe src={currentDetail.src} width={1320} height={915} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen />}
       <button className="border-2 border-main-gray text-main-gray disabled:border-main-gray100 disabled:text-main-gray100 w-[62px] h-[62px] rounded-full flex justify-center items-center" onClick={() => setCurrentDetailIndex(currentDetailIndex + 1)} disabled={!canMoveNext}>
         <ChevronRight />
