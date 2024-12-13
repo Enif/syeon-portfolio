29e42642249e338213c27c6fe4ea653103b35043
YoungJae Kim <yjkim1803@gmail.com>
2024-12-13 22:18:21 +0900

    feat: add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { nanumSquare } from "~/app/fonts";
+import Head from "next/head";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 
 export const metadata: Metadata = {
   title: "syeon.studio",
   description: "Sooyeon's portfolio website",
 };
+
+const GTM_ID = 'GTM-T2GBXKP7';
 
 export default function RootLayout({
   children,
@@ -15,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <GoogleTagManager gtmId={GTM_ID} />
+      </Head>
       <body
         className={`${nanumSquare.className} antialiased bg-white`}
       >
