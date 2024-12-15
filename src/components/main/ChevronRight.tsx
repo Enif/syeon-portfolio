706115f98cc0f8ffec50bb00120605b1d4c7c70c
YoungJae Kim <yjkim1803@gmail.com>
2024-12-16 02:34:53 +0900

    feat: artwork detail 모바일 대응

--- src/components/main/ChevronRight.tsx
+++ src/components/main/ChevronRight.tsx
@@ -1,6 +1,6 @@
-export default function ChevronRight() {
+export default function ChevronRight({ className }: { className?: string }) {
   return (
-    <svg width="21" height="36" viewBox="0 0 21 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="21" height="36" viewBox="0 0 21 36" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
       <path fill-rule="evenodd" clip-rule="evenodd" d="M20.1213 18L3.06068 35.0607C2.47489 35.6465 1.52514 35.6465 0.939356 35.0607C0.353569 34.4749 0.35357 33.5251 0.939356 32.9393L15.8787 18L0.939358 3.06067L3.06068 0.939346L20.1213 18Z" fill="currentColor" />
     </svg>
   );
--- src/components/main/artworkDetail.tsx
+++ src/components/main/artworkDetail.tsx
@@ -5,6 +5,14 @@ import ChevronRight from "./ChevronRight";
 
 export type ArtworkDetailType = { type: 'image' | 'video', src: string, alt: string };
 
+const moveButtonStyle = `border-2 border-main-gray text-main-gray
+  disabled:border-main-gray100 disabled:text-main-gray100
+  w-10 h-10 md:w-[62px] md:h-[62px] active:bg-main-gray100
+  rounded-full flex justify-center items-center disabled:cursor-not-allowed
+  absolute -bottom-12 md:static md:bottom-auto`;
+
+const chevronIconStyle = `w-5 h-5 lg:w-auto lg:h-auto`;
+
 type Props = {
   artworkDetails: ArtworkDetailType[];
 }
@@ -16,17 +24,26 @@ export default function ArtworkDetail({ artworkDetails }: Props) {
   const canMovePrev = currentDetailIndex > 0;
   const canMoveNext = currentDetailIndex < artworkDetails.length - 1;
 
+  const viewPortWidth = window.innerWidth;
+  const iframeWidth = viewPortWidth < 1024 ? viewPortWidth : 1320;
+  const iframeHeight = 915 * iframeWidth / 1320;
+
   if (!currentDetail) return null;
 
   return (
-    <div className="flex items-center gap-[30px]">
-      <button className="border-2 border-main-gray text-main-gray disabled:border-main-gray100 disabled:text-main-gray100 w-[62px] h-[62px] rounded-full flex justify-center items-center" onClick={() => setCurrentDetailIndex(currentDetailIndex - 1)} disabled={!canMovePrev}>
-        <ChevronLeft />
+    <div className="flex items-center gap-[30px] relative">
+      <button
+        className={`${moveButtonStyle} left-[30px] lg:left-auto`}
+        onClick={() => setCurrentDetailIndex(currentDetailIndex - 1)}
+        disabled={!canMovePrev}>
+        <ChevronLeft className={chevronIconStyle} />
       </button>
-      {currentDetail.type === 'image' && <Image src={currentDetail.src} alt={currentDetail.alt} width={1320} height={915} />}
-      {currentDetail.type === 'video' && <iframe src={currentDetail.src} width={1320} height={915} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen />}
-      <button className="border-2 border-main-gray text-main-gray disabled:border-main-gray100 disabled:text-main-gray100 w-[62px] h-[62px] rounded-full flex justify-center items-center" onClick={() => setCurrentDetailIndex(currentDetailIndex + 1)} disabled={!canMoveNext}>
-        <ChevronRight />
+      {currentDetail.type === 'image' && <Image src={currentDetail.src} alt={currentDetail.alt} width={1320} height={915} className="w-full lg:w-[1320px]" />}
+      {currentDetail.type === 'video' && <iframe src={currentDetail.src} width={iframeWidth} height={iframeHeight} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen />}
+      <button className={`${moveButtonStyle} right-[30px]`}
+        onClick={() => setCurrentDetailIndex(currentDetailIndex + 1)}
+        disabled={!canMoveNext}>
+        <ChevronRight className={chevronIconStyle} />
       </button>
     </div>
   );
